e5fdacde879244a23a4c6c23c26a6b405098ab7e
Ulukbek14 <ulukbekkylycbekov@gmail.com>
2022-01-22 10:38:01 +0600

    Светлая/Темная тема

--- app/src/main/java/com/geektech/newsapp/ui/fragment/setting/SettingFragment.kt
+++ app/src/main/java/com/geektech/newsapp/ui/fragment/setting/SettingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import com.geektech.newsapp.R
 import com.geektech.newsapp.databinding.FragmentSettingBinding
 
@@ -12,17 +13,20 @@ class SettingFragment : Fragment() {
 
     lateinit var binding: FragmentSettingBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSettingBinding.inflate(LayoutInflater.from(context))
         return binding.root
         setupNight()
     }
 
-    private fun setupNight() {
-
+    private fun setupNight() = with(binding){
+        DarkModeScSetting.setOnClickListener{
+            if (DarkModeScSetting.isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
